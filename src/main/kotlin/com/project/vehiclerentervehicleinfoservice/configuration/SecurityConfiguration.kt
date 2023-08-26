f6e8ea989f248da73ec6e616b51cdef5cc83b271
oenegm <oenegm@gmail.com>
2023-08-26 14:53:41 +0300

    configuration

---
+++ src/main/kotlin/com/project/vehiclerentervehicleinfoservice/configuration/SecurityConfiguration.kt
@@ -0,0 +1,33 @@
+package com.project.vehiclerentervehicleinfoservice.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers(HttpMethod.OPTIONS).permitAll()
+
+                    .requestMatchers(HttpMethod.GET, "/api/v1/brands").permitAll()
+
+
+                    .anyRequest().authenticated()
+            }
+            .oauth2ResourceServer { oauth2 ->
+                oauth2
+                    .jwt(Customizer.withDefaults())
+            }
+
+        return http.build()
+    }
+}
