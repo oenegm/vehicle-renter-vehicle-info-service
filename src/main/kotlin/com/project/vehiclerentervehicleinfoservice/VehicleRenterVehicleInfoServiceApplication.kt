70f471e8865d274f8f3301008da672e10c823c03
oenegm <oenegm@gmail.com>
2023-08-27 01:18:43 +0300

    configuration

--- src/main/kotlin/com/project/vehiclerentervehicleinfoservice/VehicleRenterVehicleInfoServiceApplication.kt
+++ src/main/kotlin/com/project/vehiclerentervehicleinfoservice/VehicleRenterVehicleInfoServiceApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class VehicleRenterVehicleInfoServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<VehicleRenterVehicleInfoServiceApplication>(*args)
+    runApplication<VehicleRenterVehicleInfoServiceApplication>(*args)
 }
