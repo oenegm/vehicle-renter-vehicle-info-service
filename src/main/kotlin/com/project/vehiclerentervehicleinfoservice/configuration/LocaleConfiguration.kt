63e74eeac86bf2a2a268816c6fde15e059bcabf0
oenegm <oenegm@gmail.com>
2023-08-26 23:12:48 +0300

    add locale configuration

---
+++ src/main/kotlin/com/project/vehiclerentervehicleinfoservice/configuration/LocaleConfiguration.kt
@@ -0,0 +1,28 @@
+package com.project.vehiclerentervehicleinfoservice.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ResourceBundleMessageSource
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.util.*
+
+
+@Configuration
+class LocaleConfiguration {
+
+    @Bean
+    fun localeResolver(): LocaleResolver {
+        val localeResolver = SessionLocaleResolver()
+        localeResolver.setDefaultLocale(Locale.US)
+        return localeResolver
+    }
+
+    @Bean
+    fun messageSource(): ResourceBundleMessageSource {
+        val messageSource = ResourceBundleMessageSource()
+        messageSource.setUseCodeAsDefaultMessage(true)
+        messageSource.setBasename("messages")
+        return messageSource
+    }
+}
